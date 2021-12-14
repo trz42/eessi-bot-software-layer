05ad23b60e1cb1873c2c1bab091f9ee79962efd7
Bob Dröge <b.e.droge@rug.nl>
2021-12-14 16:24:40 +0100

    log event data as json

--- eessi_bot_software_layer.py
+++ eessi_bot_software_layer.py
@@ -38,8 +38,9 @@ def log_event(request):
     event_type = request.headers['X-GitHub-Event']
     msg_txt = '\n'.join([
         "Event type: %s" % event_type,
-        "Request headers: %s" % pprint.pformat(dict(request.headers)),
-        "Request body: %s" % pprint.pformat(request.json),
+        #"Request headers: %s" % pprint.pformat(dict(request.headers)),
+        #"Request body: %s" % pprint.pformat(request.json),
+        "Event data (JSON): %s" % json.dumps({'headers': dict(request.headers), 'json': request.json}, indent=4),
         '',
     ])
     log(msg_txt)
