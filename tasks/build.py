6c78a191655f6946a852a05c4d027802b7314a93
Thomas Roeblitz <thomas.roblitz@uib.no>
2022-06-16 20:15:19 +0000

    Code style refinements

--- tasks/build.py
+++ tasks/build.py
@@ -17,9 +17,11 @@ from datetime import datetime
 def process_job_result(pr, event_info, jobid, pr_dir):
     # structure of directory tree
     #   jobs_base_dir/YYYY.MM/pr_<id>/event_<id>/run_<id>/target_<cpuarch>
-    #   jobs_base_dir/YYYY.MM/pr_<id>/<jobid> - being a link to the job dir
+    #   jobs_base_dir/YYYY.MM/pr_<id>/<jobid> - being a symlink to the job dir
     #   jobs_base_dir/YYYY.MM/pr_<id>/<jobid>/slurm-<jobid>.out
+
     gh = github.get_instance()
+
     # seems we have to use `base` here (instead of `head`)
     repo_name = pr.base.repo.full_name
     log("process_job_result: repo_name %s" % repo_name)
@@ -29,7 +31,7 @@ def process_job_result(pr, event_info, jobid, pr_dir):
 
     job_dir = os.path.join(pr_dir,jobid)
     sym_dst = os.readlink(job_dir)
-    slurm_out = os.path.join(sym_dst,'slurm-%s.out' % (jobid))
+    slurm_out = os.path.join(sym_dst,'slurm-%s.out' % jobid)
 
     # determine all tarballs that are stored in the job directory (only expecting 1)
     tarball_pattern = 'eessi-*software-*.tar.gz'
@@ -73,7 +75,7 @@ def process_job_result(pr, event_info, jobid, pr_dir):
         comment = 'FAILURE The build job (directory %s -> %s) encountered some issues:\n' % (job_dir,sym_dst)
         if not os.path.exists(slurm_out):
             # no slurm out ... something went wrong with the job
-            comment += ' - Did not find slurm output file (%s).\n' % (slurm_out)
+            comment += ' - Did not find slurm output file (%s).\n' % slurm_out
         if not no_missing_modules:
             # Found slurm out, but doesn't contain message 'No missing modules!'
             comment += ' - Slurm output file (%s) does not contain pattern: %s.\n' % (slurm_out,re_missing_modules.pattern)
@@ -162,18 +164,18 @@ def build_easystack_from_pr(pr, event_info):
                                      stderr=subprocess.PIPE)
         log("Cloned repo!\nStdout %s\nStderr: %s" % (cloned_repo.stdout,cloned_repo.stderr))
 
-        cmd = 'curl -L https://github.com/%s/pull/%s.patch > %s.patch' % (repo_name,pr.number,pr.number)
-        log("Obtain patch by running '%s' in directory '%s'" % (cmd,arch_job_dir))
-        got_patch = subprocess.run(cmd,
+        curl_cmd = 'curl -L https://github.com/%s/pull/%s.patch > %s.patch' % (repo_name,pr.number,pr.number)
+        log("Obtain patch by running '%s' in directory '%s'" % (curl_cmd,arch_job_dir))
+        got_patch = subprocess.run(curl_cmd,
                                    cwd=arch_job_dir,
                                    shell=True,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
         log("Got patch!\nStdout %s\nStderr: %s" % (got_patch.stdout,got_patch.stderr))
 
-        cmd = 'git am %s.patch' % pr.number
-        log("Apply patch by running '%s' in directory '%s'" % (cmd,arch_job_dir))
-        patched = subprocess.run(cmd,
+        git_am_cmd = 'git am %s.patch' % pr.number
+        log("Apply patch by running '%s' in directory '%s'" % (git_am_cmd,arch_job_dir))
+        patched = subprocess.run(git_am_cmd,
                                  cwd=arch_job_dir,
                                  shell=True,
                                  stdout=subprocess.PIPE,
@@ -193,9 +195,16 @@ def build_easystack_from_pr(pr, event_info):
     for job in jobs:
         log("Submit job with '%s %s %s %s' from directory '%s'" % (submit_command, job[2], build_job_script, local_tmp, job[0]))
         # TODO make local_tmp specific to job? to isolate jobs if multiple ones can run on a single node
-        command_line = submit_command + ' ' + job[2] + ' ' + build_job_script + ' ' + local_tmp;
+        command_line = ' '.join([
+            submit_command,
+            job[2],
+            build_job_script,
+            local_tmp,
+        ])
         submitted = subprocess.run(command_line, shell=True, cwd=job[0], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # parse job id & add it to array of submitted jobs PLUS create a symlink from main pr_<ID> dir to job dir (job[0])
+
+        # sbatch output is 'Submitted batch job JOBID'
+        #   parse job id & add it to array of submitted jobs PLUS create a symlink from main pr_<ID> dir to job dir (job[0])
         job_id = submitted.stdout.split()[3].decode("UTF-8")
         submitted_jobs.append(job_id)
         log("jobs_base_dir: %s, ym: %s, pr_id: %s, job_id: %s" % (jobs_base_dir,ym,pr_id,job_id))
@@ -207,6 +216,7 @@ def build_easystack_from_pr(pr, event_info):
     if poll_interval <= 0:
         poll_interval = 60
     poll_command = config.get_section('buildenv').get('poll_command')
+    # the below method works if the poll command is 'squeue'
     jobs_to_be_checked = submitted_jobs.copy()
     while len(jobs_to_be_checked) > 0:
         # initial pause/sleep
@@ -215,15 +225,16 @@ def build_easystack_from_pr(pr, event_info):
         #   - handle finished jobs
         #   - update jobs_to_be_checked if any job finished
         job_list_str = ','.join(jobs_to_be_checked)
+        # TODO instead of '--long' specify explicitly which columns shall be shown
         squeue_cmd = '%s --long --jobs=%s' % (poll_command,job_list_str)
-        log("run squeue command: %s" % (squeue_cmd))
+        log("run squeue command: %s" % squeue_cmd)
         squeue = subprocess.run(squeue_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # cases
         # (1) no job returned -> all finished -> check result for each, update jobs_to_be_checked
         # (2) some/not all jobs returned -> may check status (to detect potential issues), check result for jobs not returned & update jobs_to_be_checked for them
         # (3) all jobs returned -> may check status (to detect potential issues)
         log("squeue output: +++%s+++" % squeue.stdout)
-        # TODO sanity check if any output from command
+        # TODO add sanity check if any output from command
         job_table = pd.read_csv(StringIO(squeue.stdout.decode("UTF-8")),delim_whitespace=True,skiprows=1)
         if len(job_table) == 0:
             # case (1)
@@ -242,7 +253,7 @@ def build_easystack_from_pr(pr, event_info):
             finished = jtbc_df[~jtbc_df["JOBID"].isin(job_table["JOBID"])]
             log("Some jobs seem finished: '%s'" % (','.join(finished["JOBID"]).tolist()))
             for fj in finished["JOBID"].tolist():
-                log("Processing result of job '%s'." % (fj))
+                log("Processing result of job '%s'." % fj)
                 process_job_result(pr, event_info, fj, os.path.join(jobs_base_dir, ym, pr_id))
                 jobs_to_be_checked.remove(fj)
             # set B: not yet finished
