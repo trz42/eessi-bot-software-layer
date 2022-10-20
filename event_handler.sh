1e308f16304b5f926c5c1583f0a367301d553d19
hafsanaeem-2 <hafsa.na33m321@gmail.com>
2022-10-20 13:43:43 +0000

    Modification  in comments for accurate file name

--- event_handler.sh
+++ event_handler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Usage: run with ./event_handler.sh from directory where eessi-bot-event-handler.py is located
+# Usage: run with ./event_handler.sh from directory where eessi_bot_event_handler.py is located
 python3 -m eessi_bot_event_handler "$@"
