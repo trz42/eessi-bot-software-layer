a66fa26efa960085dd4b3530434e4fe95d7c72b5
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-10-27 20:00:10 +0200

    add test for mkdir function + CI workflow to run tests with Python 3.6 - 3.11

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This file is part of PyGHee (pronounced as "piggy"), the GitHub Event Executor,
+# is a GitHub App to process GitHub events, implemented in Python;
+# see https://github.com/boegel/pyghee
+#
+# author: Kenneth Hoste (@boegel)
+#
+# license: GPLv2
+#
+PYTHONPATH=$PWD:$PYTHONPATH pytest -v -s
