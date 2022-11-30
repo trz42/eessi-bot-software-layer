ee5496fb07f4ea7631cacb4b35a360accffe3e9b
hafsanaeem-2 <hafsa.na33m321@gmail.com>
2022-11-30 09:43:26 +0000

    import fixed

--- eessi_bot_event_handler.py
+++ eessi_bot_event_handler.py
@@ -17,7 +17,7 @@ import waitress
 
 from connections import github
 from tools import config
-from tools.args import parse_common_args, event_handler_parse
+from tools.args import event_handler_parse
 from tasks.build import submit_build_jobs
 from tasks.deploy import deploy_built_artefacts
 
