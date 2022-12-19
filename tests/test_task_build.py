fedacb8841d807de2c46b906d3e4af2ef9ad80ee
Thomas Roeblitz <thomas.roblitz@uib.no>
2022-12-19 21:46:24 +0100

    fixing second fail test that raises an exception

--- tests/test_task_build.py
+++ tests/test_task_build.py
@@ -26,18 +26,19 @@ def test_run_cmd(tmpdir):
     assert output == "hello\n"
     assert err == ""
 
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(Exception):
         output, err,  exit_code = run_cmd("ls -l /does_not_exists.txt", 'fail test', tmpdir, log_file=log_file)
 
         assert exit_code != 0
         assert output == ""
         assert "No such file or directory" in err
 
-    output, err, exit_code = run_cmd("this_command_does_not_exist", 'fail test', tmpdir, log_file=log_file)
+    with pytest.raises(Exception):
+        output, err, exit_code = run_cmd("this_command_does_not_exist", 'fail test', tmpdir, log_file=log_file)
 
-    assert exit_code != 0
-    assert output == ""
-    assert ("this_command_does_not_exist: command not found" in err or "this_command_does_not_exist: not found" in err)
+        assert exit_code != 0
+        assert output == ""
+        assert ("this_command_does_not_exist: command not found" in err or "this_command_does_not_exist: not found" in err)
 
     output, err, exit_code = run_cmd("echo hello", "test in file", tmpdir, log_file=log_file)
     with open(log_file, "r") as fp:
