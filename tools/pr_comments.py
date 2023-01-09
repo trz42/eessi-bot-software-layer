61f96fdca43afed58a185f60bad380e63b6367c0
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-01-09 21:34:57 +0100

    using retry decorator to avoid duplication of code

--- tools/pr_comments.py
+++ tools/pr_comments.py
@@ -12,10 +12,10 @@
 # license: GPLv2
 #
 import re
-import time
-from tools import logging
+from retry import retry
 
 
+@retry(Exception, tries=3, delay=1)
 def get_comment(pr, search_pattern):
     """get comment using the search pattern
 
@@ -26,33 +26,19 @@ def get_comment(pr, search_pattern):
     Returns:
         comment (string): comment for the submitted job
     """
-    tries = 3
-    for i in range(tries):
-        # If the config keys are not set, get_access_token will raise a NotImplementedError
-        # Returning NoneType token will stop the connection in get_instance
-        try:
-            comments = pr.get_issue_comments()
-            for comment in comments:
+    # If the config keys are not set, get_access_token will raise a NotImplementedError
+    # Returning NoneType token will stop the connection in get_instance
+    comments = pr.get_issue_comments()
+    for comment in comments:
+        cms = f".*{search_pattern}.*"
+        comment_match = re.search(cms, comment.body)
+        if comment_match:
+            return comment
 
-                cms = f".*{search_pattern}.*"
-
-                comment_match = re.search(cms, comment.body)
-
-                if comment_match:
-                    return comment
-            break
-
-        except Exception as err:
-            if i < tries - 1:
-                n = 0.8
-                t = n*(i+1)
-                time.sleep(t)
-                continue
-            else:
-                logging.error(err)
     return None
 
 
+@retry(Exception, tries=3, delay=1)
 def get_submitted_job_comment(pr, job_id):
     """get comment of the submitted job id
 
@@ -69,24 +55,11 @@ def get_submitted_job_comment(pr, job_id):
     # NOTE adjust search string if format changed by event
     #      handler (separate process running
     #      eessi_bot_event_handler.py)
-    tries = 3
-    for i in range(tries):
-        try:
-            job_search_pattern = f"submitted.*job id `{job_id}`"
-            break
-
-        except Exception as err:
-            if i < tries - 1:
-                n = 0.8
-                t = n*(i+1)
-                time.sleep(t)
-                continue
-            else:
-                logging.error(err)
-
+    job_search_pattern = f"submitted.*job id `{job_id}`"
     return get_comment(pr, job_search_pattern)
 
 
+@retry(Exception, tries=3, delay=1)
 def update_comment(cmnt_id, pr, update):
     """update comment of the job
 
@@ -95,18 +68,5 @@ def update_comment(cmnt_id, pr, update):
         pr (object): data of pr
         update (string): updated comment
     """
-    tries = 3
-    for i in range(tries):
-        try:
-            issue_comment = pr.get_issue_comment(cmnt_id)
-            issue_comment.edit(issue_comment.body + update)
-            break
-
-        except Exception as err:
-            if i < tries - 1:
-                n = 0.8
-                t = n*(i+1)
-                time.sleep(t)
-                continue
-            else:
-                logging.error(err)
+    issue_comment = pr.get_issue_comment(cmnt_id)
+    issue_comment.edit(issue_comment.body + update)
