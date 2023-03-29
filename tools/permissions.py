0a2c678b825b673329863a7d3b7a03dc56cabf11
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-03-29 10:03:02 +0200

    new module providing functions for managing permissions

---
+++ tools/permissions.py
@@ -0,0 +1,48 @@
+# This file is part of the EESSI build-and-deploy bot,
+# see https://github.com/EESSI/eessi-bot-software-layer
+#
+# The bot helps with requests to add software installations to the
+# EESSI software layer, see https://github.com/EESSI/software-layer
+#
+# author: Thomas Roeblitz (@trz42)
+#
+# license: GPLv2
+#
+import sys
+
+from pyghee.utils import log
+from tools import config
+
+BOT_CONTROL = "bot_control"
+COMMAND_PERMISSION = "command_permission"
+
+
+def check_command_permission(pr, event_info):
+    """check if the GH account is authorized to send commands to the bot
+
+    Args:
+        pr (object): pr details
+        event_info (string): event received by event_handler
+
+    """
+    fn = sys._getframe().f_code.co_name
+
+    log(f"{fn}(): checking permission for sending commands for PR {pr.number}")
+
+    cfg = config.read_config()
+
+    bot_ctrl = cfg[BOT_CONTROL]
+
+    # verify that the GH account that has sent a command (via a adding or editing
+    # a comment) has the permission to control the bot
+    command_permission = bot_ctrl.get(COMMAND_PERMISSION, '')
+
+    log(f"{fn}(): command permission '{command_permission}'")
+
+    event_sender = event_info['raw_request_body']['sender']['login']
+    if event_sender not in command_permission.split():
+        log(f"{fn}(): GH account '{event_sender}' is not authorized to send commands to the bot instance")
+        return False
+    else:
+        log(f"{fn}(): GH account '{event_sender}' is authorized to send commands")
+        return True
