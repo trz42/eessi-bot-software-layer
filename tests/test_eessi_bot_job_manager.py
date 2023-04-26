6accc74097c18960939ba01f01ce0b682c276df5
jonas-lq <jonas.lund.qvigstad@gmail.com>
2023-04-26 14:43:09 +0200

    Added tests for job managers determine_new_jobs

--- tests/test_eessi_bot_job_manager.py
+++ tests/test_eessi_bot_job_manager.py
@@ -79,3 +79,38 @@ def test_determine_running_jobs():
         }
     }
     assert job_manager.determine_running_jobs(current_jobs_some_running) == ["0", "2"]
+
+def test_determine_new_jobs():
+    job_manager = EESSIBotSoftwareLayerJobManager()
+
+    current_jobs = {
+        '0': {
+            'jobid': '0', 'state': '', 'reason': ''
+        },
+        '1': {
+            'jobid': '1', 'state': '', 'reason': ''
+        },
+        '2': {
+            'jobid': '2', 'state': '', 'reason': ''
+        }
+    }
+    known_jobs_one_job = {
+        '0': {
+            'jobid': '0'
+        }
+    }
+    known_jobs_all_jobs = {
+        '0': {
+            'jobid': '0'
+        },
+        '1': {
+            'jobid': '1'
+        },
+        '2': {
+            'jobid': '2'
+        }
+    }
+
+    assert(job_manager.determine_new_jobs({}, current_jobs) == ['0', '1', '2'])
+    assert(job_manager.determine_new_jobs(known_jobs_one_job, current_jobs) == ['1', '2'])
+    assert(job_manager.determine_new_jobs(known_jobs_all_jobs, current_jobs) == [])
