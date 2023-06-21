75b50cf20ba19301f40e84c870afebe296382b95
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-06-21 07:05:08 +0200

    disable bot:build label + log and create PR comment

--- eessi_bot_event_handler.py
+++ eessi_bot_event_handler.py
@@ -238,11 +238,23 @@ class EESSIBotSoftwareLayer(PyGHee):
         self.log("Process PR labeled event: PR#%s, label '%s'", pr.number, label)
 
         if label == "bot:build":
-            # run function to build software stack
-            if check_build_permission(pr, event_info):
-                # use an empty filter
-                submit_build_jobs(pr, event_info, EESSIBotActionFilter(""))
+            msg = "Handling the label 'bot:build' is disabled. Use the command `bot: build [FILTER]*` instead."
+            self.log(msg)
 
+            repo_name = request_body['repository']['full_name']
+            pr_number = request_body['pull_request']['number']
+            app_name = self.cfg[GITHUB][APP_NAME]
+            command_response_fmt = self.cfg[BOT_CONTROL][COMMAND_RESPONSE_FMT]
+            comment_body = command_response_fmt.format(
+                app_name=app_name,
+                comment_response=msg,
+                comment_result=''
+            )
+            create_comment(repo_name, pr_number, comment_body)
+            # # run function to build software stack
+            # if check_build_permission(pr, event_info):
+            #     # use an empty filter
+            #     submit_build_jobs(pr, event_info, EESSIBotActionFilter(""))
         elif label == "bot:deploy":
             # run function to deploy built artefacts
             deploy_built_artefacts(pr, event_info)
