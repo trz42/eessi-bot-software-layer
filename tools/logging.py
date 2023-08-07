8e6084af2b5d2c860944f2159d67b6cc1f0497ff
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-08-07 10:09:39 +0200

    logging.py: removed unused function (and import used by that function)

--- tools/logging.py
+++ tools/logging.py
@@ -11,7 +11,6 @@
 
 # Standard library imports
 import datetime
-import json
 import os
 import sys
 
@@ -51,18 +50,3 @@ def log(msg):
     with open(LOG, 'a') as fh:
         timestamp = datetime.datetime.now().strftime("%Y%m%d-T%H:%M:%S")
         fh.write('[' + timestamp + '] ' + msg + '\n')
-
-
-def log_event(request):
-    """
-    Log event data
-    """
-    event_type = request.headers['X-GitHub-Event']
-    msg_txt = '\n'.join([
-        "Event type: %s" % event_type,
-        # "Request headers: %s" % pprint.pformat(dict(request.headers)),
-        # "Request body: %s" % pprint.pformat(request.json),
-        "Event data (JSON): %s" % json.dumps({'headers': dict(request.headers), 'json': request.json}, indent=4),
-        '',
-    ])
-    log(msg_txt)
