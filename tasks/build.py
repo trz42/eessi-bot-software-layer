99f42b1daab8170e80c5f6e370f03ce7c8fcab4a
sam <sammoors@gmail.com>
2024-09-01 12:53:36 +0200

    add support for updating submit options

--- tasks/build.py
+++ tasks/build.py
@@ -658,6 +658,20 @@ def submit_job(job, cfg):
     else:
         time_limit = f"--time={DEFAULT_JOB_TIME_LIMIT}"
 
+    # update job.slurm_opts with det_submit_opts(job) in det_submit_opts.py if available
+    do_update_slurm_opts = False
+    sys.path.append(job.working_dir)
+
+    try:
+        from det_submit_opts import det_submit_opts  # pylint:disable=import-outside-toplevel
+        do_update_slurm_opts = True
+    except ImportError:
+        log(f"{fn}(): not updating job.slurm_opts: cannot import function det_submit_opts from module det_submit_opts")
+
+    if do_update_slurm_opts:
+        job = job._replace(slurm_opts=det_submit_opts(job))
+        log(f"{fn}(): updated job.slurm_opts: {job.slurm_opts}")
+
     command_line = ' '.join([
         build_env_cfg[config.BUILDENV_SETTING_SUBMIT_COMMAND],
         build_env_cfg[config.BUILDENV_SETTING_SLURM_PARAMS],
